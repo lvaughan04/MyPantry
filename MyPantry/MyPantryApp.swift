d88d9892e08d8bb70aa01c57d831ce80e641961b
Luke Vaughan <lukeevaughan@gmail.com>
2024-11-12 22:13:08 -0600

    Initial Commit

---
+++ MyPantry/MyPantryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPantryApp.swift
+//  MyPantry
+//
+//  Created by Luke Vaughan on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MyPantryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyPantryTests/MyPantryTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyPantryTests.swift
+//  MyPantryTests
+//
+//  Created by Luke Vaughan on 11/12/24.
+//
+
+import Testing
+@testable import MyPantry
+
+struct MyPantryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
